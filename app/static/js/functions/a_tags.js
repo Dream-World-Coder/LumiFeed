3944c8debad0519907951e015403dd28cefa33bb
Subhajit Gorai <goraisubhajit345@gmail.com>
2024-09-14 23:20:08 +0530

    -*-

--- app/static/js/functions/a_tags.js
+++ app/static/js/functions/a_tags.js
@@ -1,37 +1,35 @@
-
 // changed a to span
 
-document.addEventListener('DOMContentLoaded', () => {
-    const NOTatags = document.querySelectorAll('.res__serial');
-    // const topPosition_of_searchDiv = parseInt(window.getComputedStyle(document.getElementById('queryy')).top, 10); 
+document.addEventListener("DOMContentLoaded", () => {
+  const NOTatags = document.querySelectorAll(".res__serial");
+  // const topPosition_of_searchDiv = parseInt(window.getComputedStyle(document.getElementById('queryy')).top, 10);
 
-    NOTatags.forEach((a) => {
-        a.addEventListener('click', () => {
-            // event.preventDefault();
-            // let href = a.href;
-            let href = a.getAttribute('data-href');
-            // let id = href.split('#')[1];
-            let id = href;
-            let target_elem = document.getElementById(id);
-            let t2 = target_elem.parentElement;
-            let rect = t2.getBoundingClientRect();
+  NOTatags.forEach((a) => {
+    a.addEventListener("click", () => {
+      // event.preventDefault();
+      // let href = a.href;
+      let href = a.getAttribute("data-href");
+      // let id = href.split('#')[1];
+      let id = href;
+      let target_elem = document.getElementById(id);
+      let t2 = target_elem.parentElement;
+      let rect = t2.getBoundingClientRect();
 
-            window.scrollTo({
-                top: window.scrollY + rect.top - 100,
-                behavior: 'smooth'
-            });
+      window.scrollTo({
+        top: window.scrollY + rect.top - 100,
+        behavior: "smooth",
+      });
 
-            // highlighting
-            var target = target_elem.parentElement;
-            var classToAdd = 'active_highlight';
-            if (document.body.classList.contains('dark-mode')) {
-                classToAdd = 'active_highlight_dark';
-            }
-            target.classList.add(classToAdd);
-            setTimeout(() => {
-                target.classList.remove(classToAdd);
-            }, 1500);
-        });
+      // highlighting
+      var target = target_elem.parentElement;
+      var classToAdd = "active_highlight";
+      if (document.body.classList.contains("dark-mode")) {
+        classToAdd = "active_highlight_dark";
+      }
+      target.classList.add(classToAdd);
+      setTimeout(() => {
+        target.classList.remove(classToAdd);
+      }, 1500);
     });
+  });
 });
-
--- app/static/js/functions/read.js
+++ app/static/js/functions/read.js
@@ -1,24 +1,23 @@
+const read = document.getElementById("read");
+const closeBtn = document.getElementById("close-preview");
+const openBtn = document.getElementById("open-in-new-tab");
 
-const read = document.getElementById('read');
-const closeBtn = document.getElementById('close-preview');
-const openBtn = document.getElementById('open-in-new-tab');
+const readbtns = document.querySelectorAll(".rbtn");
 
-const readbtns = document.querySelectorAll('.rbtn');
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    readbtns.forEach((readbtn) => {
-        readbtn.addEventListener('click', ()=>{
-            read.style.display = 'flex';
-        })
+document.addEventListener("DOMContentLoaded", () => {
+  readbtns.forEach((readbtn) => {
+    readbtn.addEventListener("click", () => {
+      read.style.display = "flex";
     });
-    closeBtn.addEventListener('click', ()=>{
-        read.style.display = 'none';
-    });
+  });
+  closeBtn.addEventListener("click", () => {
+    read.style.display = "none";
+  });
 
-    openBtn.addEventListener('click', ()=>{
-        // read.classList.toggle('max');
-        read.style.display = 'none';
-    });
+  openBtn.addEventListener("click", () => {
+    // read.classList.toggle('max');
+    read.style.display = "none";
+  });
 });
 
 // const readFile = new Promise((resolve, reject) => {})
