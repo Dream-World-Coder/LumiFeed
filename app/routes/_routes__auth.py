3c40eed3af475eafe6afd1224d642c5d3f4db246
Subhajit Gorai <goraisubhajit345@gmail.com>
2024-09-17 16:14:08 +0530

    -**-***-**-

--- app/routes/_routes__auth.py
+++ app/routes/_routes__auth.py
@@ -10,3 +10,18 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("auth/register.html")
+
+
+# @app.route("/logout", methods=["GET", "POST"])
+# def logout():
+#     pass
+
+
+# @app.route("/forgot_password", methods=["GET", "POST"])
+# def forgot_password():
+#     return render_template("auth/forgot_password.html")
+
+
+# @app.route("/reset_password", methods=["GET", "POST"])
+# def reset_password():
+#     return render_template("auth/reset_password.html")
