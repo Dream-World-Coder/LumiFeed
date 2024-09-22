1e264c1dfc02fc1186a128c596abb10a90249828
Subhajit Gorai <goraisubhajit345@gmail.com>
2024-09-22 20:24:50 +0530

    table_make

--- app/routes/_routes__home.py
+++ app/routes/_routes__home.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_login import login_required
-from app.routes import app
+from app.routes import app, db
 
 # from app.forms import NewsForm
 
@@ -16,3 +16,9 @@ def index():
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
+
+
+@app.route("/db_create_all_123_lorem")
+def create_tables():
+    db.create_all()
+    return "Tables created successfully!"
