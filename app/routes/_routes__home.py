2cf39605dfc6df72239bcafc50c53bd5665fd10a
Subhajit Gorai <goraisubhajit345@gmail.com>
2024-09-24 12:10:38 +0530

    tests + css + some logics, now time to move to create_app() version

--- app/routes/_routes__home.py
+++ app/routes/_routes__home.py
@@ -18,7 +18,7 @@ def profile():
     return render_template("profile.html")
 
 
-# @app.route("/db_create_all_123_lorem")
-# def create_tables():
-#     db.create_all()
-#     return "Tables created successfully!"
+@app.route("/db_create_all_123_lorem")
+def create_tables():
+    db.create_all()
+    return "Tables created successfully!"
