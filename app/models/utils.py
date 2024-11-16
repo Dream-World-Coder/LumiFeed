0901f2c54a4278ea710c01e51928834aafd17a7c
Dream-World-Coder <dreamy.star.368@gmail.com>
2024-11-16 17:42:25 +0530

    some models upgarde

--- app/models/utils.py
+++ app/models/utils.py
@@ -3,20 +3,23 @@ from app.models import db
 
 user_articles = db.Table(
     'user_articles',
+    db.Column('id', db.Integer, primary_key=True),
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
     db.Column('article_id', db.Integer, db.ForeignKey('articles.id'), primary_key=True)
 )
 
 user_collections = db.Table(
     'user_collections',
+    db.Column('id', db.Integer, primary_key=True),
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('article_id', db.Integer, db.ForeignKey('articles.id'), primary_key=True)
+    db.Column('collection_id', db.Integer, db.ForeignKey('collections.id'), primary_key=True)
 )
 
 article_collections = db.Table(
     'article_collections',
-    db.Column(),
-    db.Column()
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('article_id', db.Integer, db.ForeignKey('articles.id'), primary_key=True),
+    db.Column('collection_id', db.Integer, db.ForeignKey('collections.id'), primary_key=True)
 )
 
 class CollectionType(Enum):
